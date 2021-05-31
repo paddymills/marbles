a931063f5d6b9a6f784dcbb0465d88026edce72a
paddymills <paddymills@pm.me>
2021-05-31 07:54:33 -0400

    fix install script

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-cp config/sublevel-lime /etc/nginx/sites-enabled/sublevel-lime
-cp config/sublevel-lime.service /etc/systemd/system/
-
+# python src
 mkdir /var/www/sublevel-lime
 cp -r flask/landr/* /var/www/sublevel-lime/
 
+# gunicorn
+cp config/sublevel-lime.service /etc/systemd/system/
 systemctl start sublevel-lime
 systemctl enable sublevel-lime
 
+# nginx
+cp config/sublevel-lime /etc/nginx/sites-available/sublevel-lime
+ln -s /etc/nginx/sites-available/sublevel-lime /etc/nginx/sites-enabled
 systemctl restart nginx
